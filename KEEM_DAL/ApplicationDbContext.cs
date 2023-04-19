852edfcebe1359d8bf6ec8e45186a6723c4eabba
KraSS <groverkras@gmail.com>
2023-04-19 22:11:35 +0300

    Add project files.

---
+++ KEEM_DAL/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEEM_DAL
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
