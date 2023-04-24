9961c0dd72f6891c6ac9e172c0852208cc776731
KraSS <groverkras@gmail.com>
2023-04-24 18:33:00 +0300

    Add EmissionRepository

---
+++ KEEM_DAL/Implementation/EmissionRepository.cs
@@ -0,0 +1,35 @@
+﻿using KEEM_DAL.Interfaces;
+using KEEM_Domain.Entities.Models;
+
+namespace KEEM_DAL.Implementation
+{
+    public class EmissionRepository : IBaseRepository<Emission>
+    {
+        private readonly AppDbContext _dbContext;
+
+        public EmissionRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task Create(Emission entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Emission entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Emission> GetAll()
+        {
+            return _dbContext.Emissions;
+        }
+
+        public Task Update(Emission entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
