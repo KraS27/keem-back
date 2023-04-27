a7f6554456a710c61c11a07d40dd9ddded60ea30
KraSS <groverkras@gmail.com>
2023-04-27 21:30:49 +0300

    Create GdkDTO

---
+++ KEEM_Domain/Entities/DTO/GdkDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEEM_Domain.Entities.DTO
+{
+    public class GdkDTO
+    {
+        public int Id { get; set; }
+
+        public double MpcAverage_D { get; set; }
+    }
+}
