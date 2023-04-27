bd47facfd097a511228802aa81b47275e6390fb3
KraSS <groverkras@gmail.com>
2023-04-27 21:23:12 +0300

    Add Gdk model

---
+++ KEEM_Domain/Entities/Models/Gdk.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEEM_Domain.Entities.Models
+{
+    public class Gdk
+    {
+        public int Id { get; set; }
+
+        public string DangerCLass { get; set; }
+
+        public int Environment { get; set; }
+
+        public double MpcAverage_D { get; set; }
+
+        public double MpcM_Ot { get; set; }
+
+        public double Tsel { get; set; }
+    }
+}
