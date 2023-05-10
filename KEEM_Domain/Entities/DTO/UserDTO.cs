4e3cd72cb63389516bf901ea697e41463468298c
KraSS <groverkras@gmail.com>
2023-05-10 14:30:46 +0300

    Add User and UserDTO models

---
+++ KEEM_Domain/Entities/DTO/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEEM_Domain.Entities.DTO
+{
+    internal class UserDTO
+    {
+        public int Id { get; set; }
+
+        public string Password { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
---
+++ KEEM_Domain/Entities/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEEM_Domain.Entities.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public int IdOfExpert { get; set; }
+
+        public string Password { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
