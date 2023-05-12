12c40eb750f520e2b861321785aa5d9b5309fd3f
KraSS <groverkras@gmail.com>
2023-05-12 19:19:43 +0300

    Add enum StatusCode

---
+++ KEEM_Domain/Entities/StatusCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEEM_Domain.Entities
+{
+    public enum StatusCode
+    {
+        Ok = 200,
+        BadRequest = 400,
+        NotFound = 404,
+        ServerError = 500,
+    }
+}
