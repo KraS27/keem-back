ff7b6a76f4d7d996b0e3f2bd1d55bf64701f2161
KraSS <groverkras@gmail.com>
2023-05-12 19:20:52 +0300

    Add AuthResponse

---
+++ KEEM_Domain/Entities/Responses/AuthResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEEM_Domain.Entities.Responses
+{
+    public class AuthResponse : BaseResponse<bool>
+    {
+        public StatusCode StatusCode { get; set; }
+    }
+}
